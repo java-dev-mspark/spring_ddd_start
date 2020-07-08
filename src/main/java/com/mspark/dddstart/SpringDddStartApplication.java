1a4db5797e586d6ed07e696de441c76e4314d334
unknown <mspark@wizvera.com>
2020-07-08 15:24:36 +0900

    init project

---
+++ src/main/java/com/mspark/dddstart/SpringDddStartApplication.java
@@ -0,0 +1,13 @@
+package com.mspark.dddstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDddStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDddStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mspark/dddstart/SpringDddStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mspark.dddstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDddStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
